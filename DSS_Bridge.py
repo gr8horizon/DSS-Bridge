4f95129c13961652aeca11adb4a11bc78c62e47e
Paul Barton <gr8horizon@gmail.com>
2021-01-05 01:57:36 -0800

    Added OSC Support
    Added python-osc thread for server
    validated sending and receiving OSC messages to MAX
    /DSS returns list of active DSS boards
    /DSS/A returns full 64 bit output state
    /DSS/A 2 toggles state of channel 02
    /DSS/A 2 4 5 7 toggles states of channels 02, 04, 05, 07

--- DSS_Bridge.py
+++ DSS_Bridge.py
@@ -4,24 +4,24 @@
 # Facilitate OSC command control
 # 12/23/20
 # P. Barton
+#
+# * makes the assumption that all /dev/cu.usbmodem* are DSS (Arduino Nano Every)
 
-import os, re, serial, time
+import os, re, serial, time, glob
 import rumps
 
 rumps.debug_mode(True)
 
 import socket 
 from pythonosc.udp_client import SimpleUDPClient
+from pythonosc.osc_server import ThreadingOSCUDPServer
+from pythonosc.dispatcher import Dispatcher
+import threading
 
-ip = socket.gethostbyname(socket.gethostname())
-port = 8000
-
-client = SimpleUDPClient(ip, port)  # Create client
-
-client.send_message("/some/address", 123)   # Send float message
-client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
 
 # todo: add listener to /dev for ttyusbmodem changes and store serial numbers of arduinos. lightweight polling?
+
+
 
 class DSSBridgeApp(object):
 	def __init__(self):
@@ -31,45 +31,87 @@ class DSSBridgeApp(object):
 		self.find_DSS_button = rumps.MenuItem(title="Find DSS...", callback=self.find_DSS)
 		self.DSS_button = rumps.MenuItem(title="-", callback=None)
 		self.app.menu = [self.DSS_button, self.find_DSS_button]
+		self.DSS = [];
 		self.find_DSS()
+		print(self.DSS)
+
 
 	def find_DSS(self, *etc):
 		# todo: keep track of: dev, DSS_ID, state, time queried, last request
 		DSS_IDs = []
-		device_names = os.listdir("/dev")
-		r = re.compile("cu.usbmodem.*")  # tty. doesn't give you exclusive access (cu. does)
-		port_names = [m.group(0) for m in map(r.match, device_names) if m is not None]
+		port_names = glob.glob("/dev/cu.usbmodem*")
 
 		for port_name in port_names:
 			try:
-				s = serial.Serial(port=("/dev/" + port_name), baudrate=57600, dsrdtr=True, timeout=1)
+				s = serial.Serial(port=(port_name), baudrate=57600, dsrdtr=True, timeout=1)
 			except serial.SerialException: 
-				rumps.alert(title="Audium DSS Bridge", message="Could not open Serial Port:\n\n /dev/"+port_name, icon_path="Audium_Logo_Question.png")
+				rumps.alert(title="Audium DSS Bridge", 
+					message="Could not open Serial Port:\n\n" + port_name, 
+					icon_path="Audium_Logo_Question.png")
 				pass
-				break 
+				continue 
 			time.sleep(0.2)
 			s.write(b'?\n')  # request DSS_ID from this port
-			DSS_ID = s.readline().decode()
+			DSS_ID = s.readline().decode()  # contains e.g. "A\r\n"
 			print(DSS_ID)
-			DSS_IDs.append(DSS_ID)
+			DSS_IDs.append(DSS_ID[0])
 			# s.write((DSS_ID+"\n").encode())  # request output states from DSS with this DSS_ID
 			# DSS_State = s.readline()
 			s.close()
 
 		if DSS_IDs:
 			self.DSS_button.title = "DSS Online: " + ' '.join(sorted(DSS_IDs))
-			# self.app.title = "DSS"
 			self.app.icon = "Audium_Logo.png"
+			self.DSS = dict(zip(DSS_IDs, port_names))
+			# print(self.DSS)
 		else:
 			self.DSS_button.title = '-'
-			# self.app.title = "DSS?"
 			self.app.icon = "Audium_Logo_Question.png"
-			rumps.alert(title="Audium DSS Bridge", message="No DSS Found", icon_path="Audium_Logo_Question.png")
+			rumps.alert(title="Audium DSS Bridge", 
+				message="No DSS Found", 
+				icon_path="Audium_Logo_Question.png")
 
 	def run(self):
 		self.app.run()
-		
+
+
+def filter_handler(address, *args):
+    print("OSC Message Received: " + f"{address}: {args}")
+
+def DSS_handler(address, *args):
+	client.send_message("/DSS", sorted(DSSapp.DSS.keys())) 
+
+def DSS_switcher_handler(address, *args):
+	myDSS_ID = address[-1]
+	s = serial.Serial(port=DSSapp.DSS[myDSS_ID], baudrate=57600, dsrdtr=True, timeout=1)
+
+	if len(args) > 0:
+		for arg in args:
+			s.write((myDSS_ID + ("%02d" % arg) + "\n").encode())  # toggle output state of one switch	
+			s.readline()  # dummy read (todo: turn off echo in arduino)
+
+	s.write((myDSS_ID + "\n").encode())  # request all output states from DSS
+	DSS_State = s.readline().decode()
+	client.send_message("/DSS/" + myDSS_ID, DSS_State)  # Send DSS output state
+	s.close()
+
 
 if __name__ == '__main__':
+	dispatcher = Dispatcher()
+	dispatcher.map("/DSS", DSS_handler)
+	dispatcher.map("/DSS/*", DSS_switcher_handler)
+	dispatcher.set_default_handler(print)
+
+	server_port = 1337  # OSC-Receive (into DSS_Bridge)
+	client_port = 1338  # OSC-Send (out of DSS_Bridge)
+	
+	server = ThreadingOSCUDPServer(("127.0.0.1", server_port), dispatcher)
+	server_thread = threading.Thread(target=server.serve_forever)
+	server_thread.start()
+
+	client = SimpleUDPClient("127.0.0.1", client_port)  # Create client
+
 	DSSapp = DSSBridgeApp()
 	DSSapp.run()
+
+
